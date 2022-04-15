fe1dfbe7507abd537100226fb90eb0c2d551c79a
Nathan37 <jhond999@gmail.com>
2022-04-15 17:28:22 -0300

    update description

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,14 @@ import React from "react";
 import propTypes from "prop-types"
 import "./cards.css"
 
-function Card({title,imageSrc,url}){
+function Card({title,imageSrc,url,description}){
     return(
         <div className="card text-center bg-dark">
             <img src={imageSrc} alt="Imagen"/>
             <div className="card-body">
                 <h4 className="card-title text-light">{title}</h4>
                 <p className="card-text text-secondary">
-                Vikings es una serie de televisión de drama histórico creada y escrita por Michael Hirst para el canal History. 
-                Filmada en Irlanda, se estrenó el 3 de marzo de 2013 y tuvo 6 temporadas y un total de 89 episodios.
-                Vikings está inspirada en las sagas del vikingo nórdico Ragnar Lodbrok
+                {description}
                 </p>
                 <a href={url} className="btn btn-outline-info" target="_blank">+ Info en la pagina oficial</a>
             </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,20 +9,23 @@ const cards=[
         id:1,
         title:"Vikingos",
         image:imagen1,
-        url:"https://www.sensacine.com/series/serie-10214/"
+        url:"https://www.sensacine.com/series/serie-10214/",
+        description:"Vikings es una serie de televisión de drama histórico creada y escrita por Michael Hirst para el canal History.Filmada en Irlanda, se estrenó el 3 de marzo de 2013 y tuvo 6 temporadas y un total de 89 episodios.Vikings está inspirada en las sagas del vikingo nórdico Ragnar Lodbrok"
     },
     {
         id:2,
         title:"Juego de Tronos",
         image:imagen2,
-        url:"https://es.wikipedia.org/wiki/Game_of_Thrones"
+        url:"https://es.wikipedia.org/wiki/Game_of_Thrones",
+        description:"La primera temporada comienza quince años después de la guerra civil conocida como la «rebelión de Robert», con la cual Robert Baratheon expulsó del Trono de Hierro a los Targaryen y se proclamó Rey de Poniente. Tras la repentina muerte de la Mano del Rey, Jon Arryn"
     },
     {
         id:3,
         title:"La Casa de Papel",
         image:imagen3,
-        url:"https://www.antena3.com/series/casa-de-papel/"
-    }
+        url:"https://www.antena3.com/series/casa-de-papel/",
+        description:"Un misterioso hombre conocido como «el Profesor» ha pasado toda su vida planeando el mayor de los atracos de la historia: entrar en la Fábrica Nacional de Moneda y Timbre e imprimir 2400 millones de euros recluta a un equipo de ocho personas con habilidades especiales"
+    }    
 ]
 
 function Cards(){
@@ -32,7 +35,7 @@ function Cards(){
                 {
                     cards.map(card=>(
                         <div className="col-md-4" key={card.id}>
-                            <Card title={card.title} imageSrc={card.image} url={card.url}/>
+                            <Card title={card.title} imageSrc={card.image} url={card.url} description={card.description}/>
                         </div>
                     ))
                 }    
